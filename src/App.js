b1e79b38094e9c9a5d1baf820ca880d0d6286420
McLovin1990soft <sven_ebert1337@icloud.com>
2023-11-06 12:51:45 +0100

    loader

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
+import React, { useState, useEffect } from "react";
 
 import Home from "./pages/Home/HomePage";
 import Popular from "./components/Popular/Popular";
@@ -8,16 +9,36 @@ import Child from "./components/Child/Child";
 import Family from "./components/Family/Family";
 
 function App() {
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 3500);
+  }, []);
+
   return (
-    <div>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route exact path="/popular" element={<Popular />} />
-        <Route exact path="/action" element={<Action />} />
-        <Route exact path="/child" element={<Child />} />
-        <Route exact path="/family" element={<Family />} />
-      </Routes>
-    </div>
+    <>
+      {loading ? (
+        <div className="loader">
+          <div className="ring"></div>
+          <span className="span">
+            <span>Ani</span>Lix.
+          </span>
+        </div>
+      ) : (
+        <div>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route exact path="/popular" element={<Popular />} />
+            <Route exact path="/action" element={<Action />} />
+            <Route exact path="/child" element={<Child />} />
+            <Route exact path="/family" element={<Family />} />
+          </Routes>
+        </div>
+      )}
+    </>
   );
 }
 
